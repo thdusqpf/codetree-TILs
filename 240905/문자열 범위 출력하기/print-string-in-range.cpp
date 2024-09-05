144fc3c5c31dd141cc9f5bb1aef6431357626c2f
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-05 23:47:06 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240905/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    for (int i = 2; i < 10; i++) {
+        cout << s[i];
+    }
+    return 0;
+}

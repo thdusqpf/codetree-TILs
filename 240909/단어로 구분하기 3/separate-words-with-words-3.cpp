1d6bc122868916ddecb8ee6f06508d747e5d9d73
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-09 12:42:25 +0900

    Title: 단어로 구분하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str[10];
+
+    for (int i = 0; i < 10; i++)
+        cin >> str[i];
+
+    for (int i = 9; i >= 0; i--)
+        cout << str[i] << '\n';
+    return 0;
+}

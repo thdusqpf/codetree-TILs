a0233f94e731d54fdc25b2dd2b24947686b72455
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-13 02:28:43 +0900

    Title: 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/문자열의 개수/number-of-spring.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str[200];
+    int cnt = 0;
+    for (int i = 0; i < 200; i++) {
+	cin >> str[i];
+	if (str[i][0] == '0') break;
+	cnt++;
+	}
+	
+	cout << cnt << '\n';
+	for (int i = 0; str[i][0] != '0'; i++) {
+		if (i % 2 == 0) cout << str[i] << '\n';
+	}
+    return 0;
+}

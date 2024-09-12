78440f7e790161e790cdad0339d62d895e4fa937
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-13 00:26:56 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    while (n--) {
+        int i;
+        cin >> i;
+        cout << (char)i << " ";
+    }
+    return 0;
+}

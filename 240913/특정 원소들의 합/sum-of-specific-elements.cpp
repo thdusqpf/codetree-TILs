74b1b7c0470bf6b836d90e1e69a452089381372c
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-13 23:33:53 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[4][4];
+    int sum = 0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) 
+            cin >> a[i][j];
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += a[i][j];
+        }
+    }
+
+    cout << sum;
+    return 0;
+}

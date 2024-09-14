b15f9be77eec2b721f7d984713209cffbb5bf0c7
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-14 23:54:05 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240914/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+    cin >> score;
+
+    if (score >= 80) cout << "pass";
+    else cout << 80 - score << " more score";
+    return 0;
+}

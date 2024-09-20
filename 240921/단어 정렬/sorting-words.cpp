a3e0348a713e86f8801d856179e9074f8ad4763a
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-21 08:12:35 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240921/단어 정렬/sorting-words.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s[n];
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    
+    sort(s, s + n);
+
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << '\n';
+    }
+    
+    return 0;
+}

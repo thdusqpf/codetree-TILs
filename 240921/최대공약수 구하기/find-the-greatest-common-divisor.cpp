dd3766451154f4217d4f3ff5e8dbf4ea6bc09e29
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-09-21 08:28:24 +0900

    Title: 최대공약수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int mx;
+int GCD(int n, int m) {
+    for (int i = 2; i <= min(n, m); i++) {
+        if (12 % i == 0 && 18 % i == 0) 
+            mx = i;
+    }
+    return mx;
+}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << GCD(n, m);
+
+    return 0;
+}
